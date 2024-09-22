035b5c08de2086244c264093da39ebbb3e031b79
artin <artin@cat.ms>
2024-09-22 11:26:48 +0800

    chore: update code

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use sloughi::Sloughi;
 
 fn main() {
     let _ = Sloughi::new()
-        .custom_path(".git_hooks") // Choose a custom Git hooks relative path (default is ".sloughi")
+        .custom_path(".git_hooks")
         .ignore_env("CI") // Ignore setup when `CI` environment variable is set (like in CircleCI ..etc)
         .ignore_env("GITHUB_ACTIONS") // Do not run in GitHub Actions as well
         .install();
