dfc47e42f515ec837d0c8cf83c38743104b64c1a
artin <artin@cat.ms>
2024-09-26 22:09:16 +0800

    feat: find result support select

--- src/commands/find.rs
+++ src/commands/find.rs
@@ -1,4 +1,11 @@
+use inquire::Select;
+
 use crate::{context::Context, helpers::platform};
+
+fn handle_result(path: &str) {
+    println!("Found: {}", path);
+    platform::clipboard::copy_path(path);
+}
 
 pub fn run(c: &Context, keyword: &str) {
     let result = c.database().find(keyword);
@@ -10,8 +17,18 @@ pub fn run(c: &Context, keyword: &str) {
 
     if result.len() == 1 {
         let path = result[0].fs_path();
-        println!("Found {}", path);
-        platform::clipboard::copy_path(&path);
+        handle_result(&path);
         return;
+    }
+
+    let options = result.iter().map(|r| r.fs_path()).collect::<Vec<_>>();
+
+    let ans = Select::new("Which project are you looking for?", options).prompt();
+
+    match ans {
+        Ok(choice) => {
+            handle_result(&choice);
+        }
+        Err(_) => println!("There was an error, please try again"),
     }
 }
