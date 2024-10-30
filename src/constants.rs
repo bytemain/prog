98afe0ff96ded643839bee3e5706e8b55b535de5
artin <artin@cat.ms>
2024-10-30 23:57:47 +0800

    fix: remove unused deps

--- src/constants.rs
+++ src/constants.rs
@@ -1,11 +1,8 @@
 use std::{cell::LazyCell, path::PathBuf};
 
-use indoc::indoc;
-
 use crate::helpers::path::get_config_path;
 
-pub const DEFAULT_CONFIG_TOML: &str = indoc! {r#"
-base = [
+pub const DEFAULT_CONFIG_TOML: &str = r#"base = [
 
 ]
 
@@ -13,8 +10,7 @@ base = [
 "github://" = "https://github.com/"
 "gitlab://" = "https://gitlab.com/"
 "bitbucket://" = "https://bitbucket.org/"
-"#
-};
+"#;
 
 pub const CONFIG_TOML_FILE: LazyCell<PathBuf> = LazyCell::new(|| get_config_path("config.toml"));
 
