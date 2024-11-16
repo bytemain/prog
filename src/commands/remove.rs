27a8ab18fc25736286bf50354d9102279f89bf11
artin <artin@cat.ms>
2024-11-16 16:28:48 +0800

    feat: optmize remove help message

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -5,7 +5,6 @@ use std::path::{Path, PathBuf};
 
 pub fn run(c: &Context, path: PathBuf) {
     let path_str = shellexpand::tilde(&path.to_str().unwrap()).into_owned();
-    println!("Remove: {:?}", path_str);
 
     let path = Path::new(&path_str);
     if !path.exists() {
@@ -13,14 +12,18 @@ pub fn run(c: &Context, path: PathBuf) {
         return;
     }
 
+    println!("Remove: {:?}", path.display());
+
     let ans = Confirm::new("Are you sure you want to remove this repository?")
         .with_default(false)
-        .with_help_message("Removed repository cannot be restored!")
+        .with_help_message(
+            "Remove repository will delete it from the database and remove the directory.",
+        )
         .prompt();
 
     match ans {
         Ok(true) => {
-            std::fs::remove_dir_all(&path).expect("Error removing directory");
+            std::fs::remove_dir_all(&path).expect("Error when removing directory");
             c.database_mut().remove(&path_str);
             println!("Repository removed: {:?}", path);
         }
