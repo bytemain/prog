e8f20b14bd431876cfb67d074dbccef60086fe4c
artin <artin@cat.ms>
2024-12-22 00:45:00 +0800

    feat: simplify clean confirm

--- src/commands/clean.rs
+++ src/commands/clean.rs
@@ -1,45 +1,20 @@
 use crate::context::Context;
-use crate::helpers::rand::gen_n_number;
-use inquire::ui::RenderConfig;
 use inquire::Confirm;
 
 pub fn run(c: &Context) {
-    println!("This will delete all your repos in database, won't delete your git repos.");
-    // generate random 4 number
-    let random_number = gen_n_number(4);
-    let ans = Confirm {
-        message: &format!("You're doing a very dangerous operation, are you sure you want to continue?\nType {} or no to continue.", {&random_number}),
-        starting_input: None,
-        default: Some(false),
-        placeholder: Some(&random_number),
-        help_message: None,
-        formatter: &|ans| match ans {
-            true => String::from(&random_number),
-            false => String::from("no"),
-        },
-        parser: &|ans| {
-            if ans == &random_number {
-                Ok(true)
-            } else if ans == "no" {
-                Ok(false)
-            } else {
-                Err(())
-            }
-        },
-        error_message: format!("Type with {} or no", &random_number).into(),
-        default_value_formatter: &|def| match def {
-            true => String::from(&random_number),
-            false => String::from("no"),
-        },
-        render_config: RenderConfig::default(),
-    }
-        .prompt()
-        .unwrap();
+    let ans = Confirm::new("You're cleaning all your repo records, continue?")
+        .with_default(false)
+        .with_help_message("This won't delete your git repos in the disk")
+        .prompt();
 
-    if ans {
-        c.delete_db_folder();
-        println!("Successfully clean the database.");
-    } else {
-        println!("Canceled.");
+    match ans {
+        Ok(true) => {
+            c.delete_db_folder();
+            println!("Successfully clean the database.");
+        }
+        Ok(false) => {
+            println!("Canceled.");
+        }
+        Err(_) => println!("Error with questionnaire, try again later"),
     }
 }
