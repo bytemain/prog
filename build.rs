8d644c392d93f569e813a2028e7d718cf3e7ee2e
Jiacheng <artin@cat.ms>
2025-03-13 03:54:34 +0000

    chore: remove sloughi dependency and related files

--- build.rs
+++
@@ -1,9 +0,0 @@
-use sloughi::Sloughi;
-
-fn main() {
-    let _ = Sloughi::new()
-        .custom_path(".husky")
-        .ignore_env("CI") // Ignore setup when `CI` environment variable is set (like in CircleCI ..etc)
-        .ignore_env("GITHUB_ACTIONS") // Do not run in GitHub Actions as well
-        .install();
-}
